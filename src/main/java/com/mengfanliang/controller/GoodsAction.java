3e8e5b0c44b637b80089be6fcc0c2ddb4560f19f
274162619@qq.com <274162619@qq.com>
2020-03-17 22:07:48 +0800

    增加了swagger2功能

--- src/main/java/com/mengfanliang/controller/GoodsAction.java
+++ src/main/java/com/mengfanliang/controller/GoodsAction.java
@@ -3,16 +3,21 @@ package com.mengfanliang.controller;
 import com.mengfanliang.entity.Goods;
 import com.mengfanliang.result.R;
 import com.mengfanliang.service.GoodsService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("goods")
 @CrossOrigin
+@Api("商品操作类")
 public class GoodsAction {
     @Autowired
     GoodsService service;
     @GetMapping("")
+
     public R getGoodsList(String goodsname,Integer pagenum,Integer pagesize){
         System.out.println(goodsname+pagenum+""+pagesize);
 
@@ -26,6 +31,8 @@ public class GoodsAction {
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value="删除指定id用户信息",notes = "根据id删除")
+    @ApiImplicitParam(name="id",value = "用户id",required = true,dataType = "Integer")
     public R remove(@PathVariable Integer id){
 
         return R.ok().data("删除成功",service.delete(id));
